2f4ef559c33b25c83b21cfae6ca00e023206ddc4
terexwizard <terex2004@gmail.com>
2015-10-12 18:37:51 +0700

    commit12/10/2558
    12/10/2558

--- src/java/com/scc/pay/util/NewMain.java
+++ src/java/com/scc/pay/util/NewMain.java
@@ -4,8 +4,14 @@
  */
 package com.scc.pay.util;
 
+import com.scc.f1.util.Utils;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
 
 /**
  *
@@ -24,5 +30,27 @@ public class NewMain {
         
         System.out.println(df2.format(new BigDecimal("12345")));
         
+        System.out.println(nextDayEn("20150929",1));
+    }
+    
+    public static String nextDayEn(String d,Integer i){  
+        
+        Date date = null; 
+ 
+        Calendar c = Calendar.getInstance();
+        
+        c.setTime(Utils.formatStringToDateToScreen(d));
+        c.add(Calendar.DATE, +i);
+  
+        SimpleDateFormat sm = new SimpleDateFormat("yyyyMMdd");
+        
+        try {
+            date = sm.parse(Utils.formatDateToStringToDBThai(c.getTime())); 
+        } catch (ParseException ex) {
+            java.util.logging.Logger.getLogger(CenterUtils.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        System.out.println(">>"+date);
+        return Utils.formatDateToStringToDBEn(date);
+ 
     }
 }
